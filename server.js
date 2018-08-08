6c31fe5f3bc1fc43396e23b8e0763d35501f48fc
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2018-08-08 22:15:10 +0430

    serve an html file

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 5000;
 
 const requestHandler = (req, res) => {
 
     if(req.url === '/about') {
-        res.write('welcome to about page.');
-        res.end();
-    } else if(req.url === '/') {
-        res.write('<h1>You are in home page.</h1>');
-        res.end();
-    } else {
-        res.write('Page not found.');
-        res.end();
+        // const aboutContent = fs.readFileSync('./about.html');
+        // res.write(aboutContent);
+        // res.end();
+
+        fs.readFile('./about.html', function (error, data) {
+            res.write(data);
+            res.end();
+        });
     }
 
 };
